af597d8e436a656938c4867f263b37264c73109a
Siddharth Jain <sid.thecooljain@gmail.com>
2017-03-26 16:44:27 +0530

    Add files via upload
    User Interface (incomplete)

---
+++ Here_I_am/src/here_i_am/Here_I_am.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package here_i_am;
+
+/**
+ *
+ * @author sibby
+ */
+public class Here_I_am {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new SetReminder().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
